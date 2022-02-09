34f384ae91e43a54fbcdae3e5e1accc7f671b0aa
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-02-09 20:46:15 -0300

    add: app.py alts | connection with sqlite3 success

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
-import sqlite3
 
-from flask import Flask
+from flask import Flask, grade
 
 database = "blog.db"
 secret_key = "pao_de_queijo"
@@ -14,9 +13,32 @@ def connect_db():
     return sqlite3.connect(database)
 
 
+@app.before_request
+def beforeRequest():
+    grade.db = connect_db()
+
+
+@app.teardown_request
+def endRequest(exe):
+    frade.db.close()
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route(f'welcome/{id}')
+def hy_user():
+    return f'Welcome to my site {id}'
+
+
+@app.route('/route_db')
+def show_posts():
+    sql = "SELECT title, text FROM input ORDER BY inputs"
+    cur = grade.db.execute(sql)
+    inputs = []
+    return inputs
 
 
 if __name__ == '__main__':
--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "add: .flaskenv | schema.sql | app.py alts"
+git commit -m "add: app.py alts | connection with sqlite3 success"
 git push
 git status
