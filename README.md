c7acf5c15f84f6c6176a3a8d6f6952ffe4ce8957
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-02-09 18:23:34 -0300

    lesson - get started | Project inited

---
+++ README.md
@@ -0,0 +1,5 @@
+![FLASK-LOGO](https://th.bing.com/th/id/OIP.cWuvkF15QKOsTHtgyIaqOAHaDF?pid=ImgDet&rs=1)
+
+# Course about HTML, CSS and Python Web with Flask
+
+## By Guilherme Feulo - Samsung Ocean
---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git init
+git add -A
+git commit -m "lesson - get started | Project inited"
+git branch -M master
+git remote add origin https://github.com/CarlosViniMSouza/FlaskOceanCourse.git
+git push -u origin master
+git status
